310dbd1967d7798af0c2d843e324bf0123c51026
JaQuan Banks <shadic151@gmail.com>
2022-09-20 20:26:53 -0400

    commit tree 58a7a62afd98a9d245634db7fd332736ab08e18f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards() {
+  const cards = ["Guadalupe", "Ollie", "Aki"];
+  const surprise = [];
+  for (let i = 0; i < cards.length; i++) {
+    surprise[i] = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+  }
+  return surprise;
+}
+
+function countDown() {
+  let countdown = 10;
+  while (countdown >= 0) {
+    console.log(countdown--);
+  }
+}
